c370ae41ea1f8a1b8c190b079315c4b385d66228
nwildi <N.Wildi.inf22@stud.bbbaden.ch>
2025-08-18 12:24:18 +0200

    add sound type and soundboard page

---
+++ types/sound.ts
@@ -0,0 +1,9 @@
+export type Sound = {
+    id: string;
+    title: string;
+    description?: string;
+    audio_url: string;
+    duration?: number;
+    color?: string;
+    created_date?: string;
+};
